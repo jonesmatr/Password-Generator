30bfbc5a079eb24778a5b3a9b8c7ee5a25bb8dc3
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-05-29 13:37:46 -0400

    Map out the criteria with comments to break the program into smaller tasks.

--- script.js
+++ script.js
@@ -20,9 +20,21 @@ function generatePassword() {
 
 //Make sure the password length is between 8 and 128 characters and also check if the entry is a number
 while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-  // Prompt the user to enter a valid password length if the entry is not a number or is not between 8 and 128 characters
+  // Prompt the user to enter a valid password length if the entry is not a number or is not between 8 and 128 characters.
   passwordLength = parseInt(prompt("Invalid input. Please ender a valid password length between 8 and 128 characters."));
 }
+//Create criteria for password
+
+//Create variables for the different character types (lowercase, uppercase, numeric, and special characters)
+
+//Ask for character types to use in password and confirm those choices (each choice will be a separate prompt and confirm)
+
+//Create function that checks that at least one character type is selected. Use if...else statement to check if at least one character type is selected. If not, alert the user to select at least one character type and return to the beginning of the function.
+
+//If all the criteria are met, use a function that generates password based on criteria. Use for loop to generate password. Use Math.floor(Math.random() * passwordLength) to generate random password. 
+
+//Return the generated password in the box on the screen. 
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
