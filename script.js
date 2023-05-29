71c4d1b844f421df5a12fcf091c9405b69abc85d
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-05-29 13:15:46 -0400

    Add a while loop to check if the user entered a number and that number is between 8 and 128.

--- script.js
+++ script.js
@@ -15,8 +15,14 @@ function writePassword() {
 
 // Function to generate password based on criteria
 function generatePassword() {
-  // Prompt user for password length
+  // Prompt user for password length and converts it the entry to an integer
   var passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128 characters);"));
+
+//Make sure the password length is between 8 and 128 characters and also check if the entry is a number
+while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+  // Prompt the user to enter a valid password length if the entry is not a number or is not between 8 and 128 characters
+  passwordLength = parseInt(prompt("Invalid input. Please ender a valid password length between 8 and 128 characters."));
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
