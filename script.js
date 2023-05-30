67a91b4940ce3d273b378736188207f8818baf35
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-05-30 15:12:49 -0400

    Code wasn't working. Found the += operator in the for loop for the password was written as =+. Fixed and now works.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -13,23 +14,26 @@ function writePassword() {
 
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Function to generate password based on criteria
 function generatePassword() {
   // Prompt user for password length and converts it the entry to an integer
-  var passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128 characters);"));
+  var passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128 characters):"));
 
 //Make sure the password length is between 8 and 128 characters and also check if the entry is a number
 while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
   // Prompt the user to enter a valid password length if the entry is not a number or is not between 8 and 128 characters.
-  passwordLength = parseInt(prompt("Invalid input. Please ender a valid password length between 8 and 128 characters."));
+  passwordLength = parseInt(prompt("Invalid input. Please ender a valid password length between 8 and 128 characters:"));
 }
-//Create criteria for password
 
 //Create variables for the different character types (lowercase, uppercase, numeric, and special characters)
 var includeLowercase = confirm("Do you want to include lowercase characters?");
 var includeUppercase = confirm("Do you want to include uppercase characters?");
 var includeNumeric = confirm("Do you want to include numeric characters?");
 var includeSpecial = confirm("Do you want to include special characters?");
+
 //Ask for character types to use in password and confirm those choices (each choice will be a separate prompt and confirm)
 while (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
   alert("You must select at least one character type!");
@@ -39,10 +43,10 @@ while (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpec
   includeSpecial = confirm("Include special characters?");
 }
 //Define character array for each type
-  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numericChars = "0123456789";
-  var specialChars = "!@#$%^&*()_-+=";
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChars = "0123456789";
+var specialChars = "!@#$%^&*()_-+=";
 
 //Create a variable to store character that'll be used for the password. Each character will get amended to this variable based on the length chosen by the user. 
 var characterSet = "";
@@ -55,10 +59,11 @@ if (includeSpecial) characterSet += specialChars;
 var password = "";
 for (var i = 0; i < passwordLength; i++) {
   var randomIndex = Math.floor(Math.random() * characterSet.length);
-  password =+ characterSet[randomIndex];
+  password += characterSet[randomIndex];
 }
+
 //Return the generated password in the box on the screen using the #password to located the textarea in the html. 
 return password;
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
